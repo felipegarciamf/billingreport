c90233fc9458e93f6df3f322fce0253ac1b321b4
felipegarciamf <silasfelipegarcia@gmail.com>
2021-09-10 11:10:27 -0300

    subindo projeto

---
+++ src/main/java/br/com/billingreport/billingreport/BillingreportApplication.java
@@ -0,0 +1,13 @@
+package br.com.billingreport.billingreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingreportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingreportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/billingreport/billingreport/BillingreportApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.billingreport.billingreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingreportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
